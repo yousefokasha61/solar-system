dce494e7d66a211d923de9db77621b478203d6d1
Youssef Okasha <36171027+yousefokasha61@users.noreply.github.com>
2024-12-14 02:08:23 +0200

    fix codebase

--- app.js
+++ app.js
@@ -1,87 +1,91 @@
-const path = require('path');
-const express = require('express');
-const OS = require('os');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const OS = require("os");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors')
-
+const cors = require("cors");
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/')));
-app.use(cors())
+app.use(express.static(path.join(__dirname, "/")));
+app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+mongoose.connect(
+  "mongodb+srv://yousefokasha61:lDHt4qOn1fikEgRL@cluster0.bkixm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    user: "yousefokasha61",
+    pass: "lDHt4qOn1fikEgRL",
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
+    useUnifiedTopology: true,
+  },
+  function (err) {
     if (err) {
-        console.log("error!! " + err)
+      console.log("error!! " + err);
     } else {
-       console.log("MongoDB Connection Successful")
+      console.log("MongoDB Connection Successful");
     }
-})
+  }
+);
 
 var Schema = mongoose.Schema;
 
 var dataSchema = new Schema({
-    name: String,
-    id: Number,
-    description: String,
-    image: String,
-    velocity: String,
-    distance: String
+  name: String,
+  id: Number,
+  description: String,
+  image: String,
+  velocity: String,
+  distance: String,
 });
-var planetModel = mongoose.model('planets', dataSchema);
+var planetModel = mongoose.model("planets", dataSchema);
 
-
-
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
-    planetModel.findOne({
-        id: req.body.id
-    }, function(err, planetData) {
-        if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
-        } else {
-            res.send(planetData);
-        }
-    })
-})
-
-app.get('/',   async (req, res) => {
-    res.sendFile(path.join(__dirname, '/', 'index.html'));
+app.post("/planet", function (req, res) {
+  // console.log("Received Planet ID " + req.body.id)
+  planetModel.findOne(
+    {
+      id: req.body.id,
+    },
+    function (err, planetData) {
+      if (err) {
+        alert(
+          "Ooops, We only have 9 planets and a sun. Select a number from 0 - 9"
+        );
+        res.send("Error in Planet Data");
+      } else {
+        res.send(planetData);
+      }
+    }
+  );
 });
 
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/", "index.html"));
+});
 
-app.get('/os',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "os": OS.hostname(),
-        "env": process.env.NODE_ENV
-    });
-})
+app.get("/os", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.send({
+    os: OS.hostname(),
+    env: process.env.NODE_ENV,
+  });
+});
 
-app.get('/live',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "status": "live"
-    });
-})
+app.get("/live", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.send({
+    status: "live",
+  });
+});
 
-app.get('/ready',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "status": "ready"
-    });
-})
+app.get("/ready", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.send({
+    status: "ready",
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
-})
-
+  console.log("Server successfully running on port - " + 3000);
+});
 
 module.exports = app;
